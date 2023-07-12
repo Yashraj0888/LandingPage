5dd654cef6fa3ba83fb73c4ca64161dd5fcb3767
Yashraj0888 <bloodymoonknite888@gmail.com>
2023-07-13 03:57:35 +0530

    More Styling

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const formOpenBtn=document.querySelector("#form-open"),
+signupbtn=document.querySelector("#form-open1"),
 home=document.querySelector(".home"),
 formContainer=document.querySelector(".form_container"),
 formClose=document.querySelector(".form_close"),
 loginBtn=document.querySelector("#login"),
 signup=document.querySelector("#signup"),
 pwShow=document.querySelectorAll(".hide");
+
+signupbtn.addEventListener("click",()=>home.classList.add("signup"))
+formClose.addEventListener("click",()=>home.classList.remove("signup"))
+loginBtn.addEventListener("click",()=>home.classList.add("login"))
+loginBtn.addEventListener("click",()=>home.classList.remove("signup"))
+formClose.addEventListener("click",()=>home.classList.remove("login"))
 
 formOpenBtn.addEventListener("click",()=>home.classList.add("show"))
 formClose.addEventListener("click",()=>home.classList.remove("show"))
@@ -35,4 +42,15 @@ loginBtn.addEventListener("click",(e)=>{
     formContainer.classList.remove("active");
 })
 
+loginBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    formContainer.classList.add("unactive");
+})
+
+signup.addEventListener("click",(e)=>{
+    e.preventDefault();
+    formContainer.classList.remove("unactive");
+    })
+formClose.addEventListener("click",()=> formContainer.classList.remove("unactive"))
+formClose.addEventListener("click",()=>home.classList.remove("active"))
 
